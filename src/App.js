1bcfa6374ab026da0862b268c3044ac747f88bd7
A-Marvulle <alfredo.marvulle@gmail.com>
2024-01-09 10:20:31 -0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react"
 import './App.css';
 
 function App() {
+  const [quoteInfo, setQuoteInfo] = useState({})
+  useEffect(() => {
+    getQuote();
+  },[])
+
+  const getQuote = () =>{
+    fetch('https://api.quotable.io/random')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) =>{
+        setQuoteInfo({
+          text: data.content,
+          author: data.author
+        })
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="quote-box">
+        <p id="text" className="quote-text">{quoteInfo.text}</p>
+        <p id="author" className="quote-author">{quoteInfo.author}</p>
+        <div className="buttons">
+          <button id="new-quote" className="button" onClick={getQuote}>New Quote</button>
+          <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + quoteInfo.text + quoteInfo.author} id="tweet-quote" target="_blank"><i class="fa fa-twitter"></i></a>
+          <a href={'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + quoteInfo.text + quoteInfo.author} id="tumblr-quote" target="_blank"><i class="fa fa-tumblr"></i></a>
+        </div>
+       
+      </div>
+  
   );
 }
 
